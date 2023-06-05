6323c9744afbdebe24ec4edd58cd8b92a6aab323
Jere Arokivi <51503069+Fl4yd@users.noreply.github.com>
2023-06-05 16:10:46 +0000

    testCommit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import "./styles.css";
+import "../leaflet/leaflet"
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+
+async function drawMap()  {
+  const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+  const mapPromise = await fetch(url);
+  const data = await mapPromise.json();
+  console.log(data)
+  initMap(data)
+};
+
+const getFeature = (feature, layer) => {
+  if (!feature.properties.id) return;
+  const id = feature.properties.id;
+  console.log(id)
+  layer.bindTooltip("Hello " + id);
+}
+
+const initMap = (data) => {
+  let map = L.map('map', {
+    minZoom: -3
+  });
+
+  let geoJson = L.geoJSON(data, {
+    onEachFeature: getFeature
+  }).addTo(map)
+
+  let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: "OpenStreetMap"
+  }).addTo(map)
+
+  map.fitBounds(geoJson.getBounds());
+}
+
+drawMap()
